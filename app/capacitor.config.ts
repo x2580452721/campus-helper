89276f9fee9e6a27287415b5381d3f13843c1cae
翊歌 <2580452721@qq.com>
2026-04-15 19:03:44 +0800

    Change APK to load local H5 instead of remote URL

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -4,12 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.campus.helper',
   appName: 'Campus Helper',
   webDir: 'www',
-  server: {
-    // 使用 Vercel 上已部署的 H5 地址
-    url: 'https://campus-helper-miniapp.vercel.app',
-    cleartext: true,
-    androidScheme: 'https'
-  },
   android: {
     allowMixedContent: true,
     captureInput: true,
